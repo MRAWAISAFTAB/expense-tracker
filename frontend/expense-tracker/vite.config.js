3b514afdf62ec69cec28e4f61c1598c0198024de
MRAWAISAFTAB <aftabawais36@gmail.com>
2026-02-26 15:26:37 +0500

    addd

--- frontend/expense-tracker/vite.config.js
+++ frontend/expense-tracker/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // increase to 1MB
-  },
-});
+  plugins: [react() , tailwindcss()],
+})
